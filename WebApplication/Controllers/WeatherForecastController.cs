a8b6401778bbb895b45eb53e4c01802594cb6204
Липчану Алексей <al1g18@yandex.com>
2021-04-08 13:02:53 +0300

    Proper Project Structure

---
+++ WebApplication/Controllers/WeatherForecastController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatabaseAccess;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EfCoreController : ControllerBase
+    {
+        GenericRepository<User> repo = new GenericRepository<User>(new ApplicationContext());
+
+        private readonly ILogger<EfCoreController> _logger;
+
+        public EfCoreController(ILogger<EfCoreController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpPost("CreateUser")]
+        public async void CreateUser(string username, string password)
+        {
+            
+            var temp = new User
+            {
+                Username = username,
+                Password = password
+            };
+            await repo.Create(temp);
+        }
+
+        [HttpGet("GetById")]
+        public async Task<User> GetById(int id)
+        {
+            
+            return await repo.GetById(id);
+        }
+
+        [HttpPost("UpdateUser")]
+        public async void UpdateUser(int id, string password)
+        {
+            var user = await repo.GetById(id);
+            user.Password = password;
+            await repo.Update(user);
+        }
+
+        [HttpPost("DeleteUser")]
+        public async void Delete(int id)
+        {
+             await repo.Delete(id);
+        }
+
+        [HttpPost("Concurrency")]
+        public async void Concurrency()
+        {
+            try
+            {
+                var anotherRepo = new GenericRepository<User>(new ApplicationContext());
+                var user = await repo.GetById(5);
+                var sameUser = await repo.GetById(5);
+                user.Password = "1234";
+                user.Password = "12345";
+                await repo.Update(user);
+                await anotherRepo.Update(user);
+            } catch(DbUpdateConcurrencyException e)
+            {
+               
+            }
+
+            
+        }
+    }
+}
---
+++ WebApplication/Migrations/20210408095157_TablePerType.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class TablePerType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CardioRoutine");
+
+            migrationBuilder.DropTable(
+                name: "GymRoutine");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Routines",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Duration",
+                table: "Routines",
+                type: "time",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Intensity",
+                table: "Routines",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Reps",
+                table: "Routines",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Sets",
+                table: "Routines",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Routines");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Routines");
+
+            migrationBuilder.DropColumn(
+                name: "Intensity",
+                table: "Routines");
+
+            migrationBuilder.DropColumn(
+                name: "Reps",
+                table: "Routines");
+
+            migrationBuilder.DropColumn(
+                name: "Sets",
+                table: "Routines");
+
+            migrationBuilder.CreateTable(
+                name: "CardioRoutine",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Intensity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardioRoutine", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CardioRoutine_Routines_Id",
+                        column: x => x.Id,
+                        principalTable: "Routines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GymRoutine",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Reps = table.Column<int>(type: "int", nullable: false),
+                    Sets = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GymRoutine", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GymRoutine_Routines_Id",
+                        column: x => x.Id,
+                        principalTable: "Routines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+        }
+    }
+}
